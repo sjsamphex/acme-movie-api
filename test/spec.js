40eb42b68436a9b6558284e44faaafb3a30bd942
sjsamphex <sjsamphex@gmail.com>
2021-02-01 17:31:55 -0800

    pg involvement

--- test/spec.js
+++ test/spec.js
@@ -1,9 +1,20 @@
 const { expect } = require('chai');
+const { syncAndSeed } = require('../db');
 
 const app = require('supertest')(require('../app'));
 
 describe('Routes', () => {
   describe('GET /', () => {
+    it('show info about api', async () => {
+      const response = await app.get('/');
+      expect(response.status).to.equal(200);
+    });
+  });
+});
+
+describe('Database', () => {
+  before(() => syncAndSeed());
+  describe('will syncandseed', () => {
     it('show info about api', async () => {
       const response = await app.get('/');
       expect(response.status).to.equal(200);
