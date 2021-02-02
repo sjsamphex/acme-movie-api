1ebc23c64afb458d5d3ccecdc31abe259683cc36
sjsamphex <sjsamphex@gmail.com>
2021-02-01 17:44:21 -0800

    db findalls routes

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
-
+const {
+  models: { Actor, Movie, Role },
+} = require('./db');
 const app = express();
 
 module.exports = app;
@@ -8,10 +10,10 @@ app.get('/', (req, res, next) => {
   res.send(`hello world`);
 });
 
-app.get('/api/movies', (req, res, next) => {
-  res.send(`hello world`);
+app.get('/api/movies', async (req, res, next) => {
+  res.send(await Movie.findAll());
 });
 
-app.get('/api/actors', (req, res, next) => {
-  res.send(`hello world`);
+app.get('/api/actors', async (req, res, next) => {
+  res.send(await Actor.findAll());
 });
